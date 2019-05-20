0916a55da98dce6572af2bcf1cae38290cc31efd
Jules LE BRIS <jls.lebris@gmail.com>
2019-05-20 09:50:33 +0200

    use webcam

--- main.cpp
+++ main.cpp
@@ -8,6 +8,16 @@ int main(){
         if(test==false){
             throw -101;
         }
+        cv::Mat img;
+        cv::namedWindow("Window2", cv::WINDOW_AUTOSIZE);
+        while(true){
+            videoOpenCv.read(img);
+            cv::imshow("window2", img);
+            int key = cv::waitKey(1000/25);
+            if(key==(int)'q'){
+                break;
+            }
+        }
     }
     catch(int e)
     {
